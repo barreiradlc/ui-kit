d5fc5683084497396d1d7a10238c1b2b6f6e34bf
barreiradlc ( Augusto Barreira ) <barreira266@hotmail.com>
2025-06-02 11:29:37 -0300

    Update the ui kit in every interaction

--- Dockerfile
+++ Dockerfile
@@ -7,16 +7,10 @@ COPY ./curiosity-ui ./curiosity-ui
 COPY ./demo ./demo
 
 # WORKDIR /app/curiosity-ui
-RUN cd /app/curiosity-ui && npm install && npm link
+RUN cd /app/curiosity-ui && npm install && npm link && npm run build
 
 # WORKDIR /app/demo
 RUN cd /app/demo && npm install && npm link "curiosity-ui"
-
-
-RUN cd /app/curiosity-ui && npm run build
-
-RUN cd /app/demo 
-
 
 EXPOSE 5173
 
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // 👈 Listen on all interfaces
     port: 5173,       // Optional: can be omitted, defaults to 5173
+    watch: {
+      usePolling: true,         // 👈 Needed for Docker + volume changes
+    },
+  },  
+   resolve: {
+    alias: {
+      'curiosity-ui': path.resolve(__dirname, '../curiosity-ui/src'),
+    },
   },
 })
